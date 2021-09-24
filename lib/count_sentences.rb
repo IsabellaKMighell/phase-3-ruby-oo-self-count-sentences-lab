4a65f168ea7368584d52b75867d719b442348dd6
Isabella Mighell <ikm4dm@virginia.edu>
2021-09-23 20:00:57 -0400

    commit tree d21b007b8e9a6857d5c6b50dd78656502b60a18a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,43 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    last_char = self[-1]
+    if last_char == "."
+      true
+    else 
+      false
+    end
   end
 
   def question?
+    last_char = self[-1]
+    if last_char == "?"
+      true
+    else 
+      false
+    end
 
   end
 
   def exclamation?
-
+    last_char = self[-1]
+    if last_char == "!"
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = 0
+    punctuation = ['.','?','!']
+    self.chars.each.with_index do |char, index|
+      if punctuation.include?(char) && !punctuation.include?(self[index+1])
+      sentences +=1
+      end
+    end
+    sentences
   end
+
 end
